9396cc5284ebfee74e0ff2f4de58d7484270e96c
Aleksey Tumbaev <leha.tumbaev@gmail.com>
2024-06-16 19:07:05 +0700

    фикс фильтров

--- src/main/java/com/example/ucomandbackend/ad/AdSpecs.java
+++ src/main/java/com/example/ucomandbackend/ad/AdSpecs.java
@@ -26,39 +26,41 @@ public class AdSpecs {
     }
 
     public Specification<Ad> userIdEqual(Long userId) {
-        return (root, query, cb) -> userId == null ? cb.conjunction() : cb.equal(root.get(Ad.F.type), userId);
+        return (root, query, cb) -> userId == null
+                ? cb.conjunction()
+                : cb.equal(root.join(Ad.F.user).get(User.F.id), userId);
     }
 
     public Specification<Ad> gendersIn(Set<Gender> genders) {
         return (root, query, cb) -> CollectionUtils.isEmpty(genders)
                 ? cb.conjunction()
-                : cb.in(root.get(Ad.F.user).get(User.F.gender)).value(genders);
+                : cb.in(root.join(Ad.F.user).get(User.F.gender)).value(genders);
     }
 
     public Specification<Ad> ageGte(Integer age) {
         return (root, query, cb) -> age == null
                 ? cb.conjunction()
-                : cb.greaterThanOrEqualTo(root.get(Ad.F.user).get(User.F.age), age);
+                : cb.greaterThanOrEqualTo(root.join(Ad.F.user).get(User.F.age), age);
     }
 
     public Specification<Ad> ageLte(Integer age) {
         return (root, query, cb) -> age == null
                 ? cb.conjunction()
-                : cb.lessThanOrEqualTo(root.get(Ad.F.user).get(User.F.age), age);
+                : cb.lessThanOrEqualTo(root.join(Ad.F.user).get(User.F.age), age);
     }
 
     public Specification<Ad> cityOfResidenceIdIn(Set<Long> cityIds) {
         return (root, query, cb) -> CollectionUtils.isEmpty(cityIds)
                 ? cb.conjunction()
-                : cb.in(root.get(Ad.F.user).get(User.F.cityOfResidence).get(City.F.id)).value(cityIds);
+                : cb.in(root.join(Ad.F.user).join(User.F.cityOfResidence).get(City.F.id)).value(cityIds);
     }
 
     public Specification<Ad> regionIdIn(Set<Long> regionIds) {
         return (root, query, cb) -> CollectionUtils.isEmpty(regionIds)
                 ? cb.conjunction()
-                : cb.in(root.get(Ad.F.user)
-                        .get(User.F.cityOfResidence)
-                        .get(City.F.region)
+                : cb.in(root.join(Ad.F.user)
+                        .join(User.F.cityOfResidence)
+                        .join(City.F.region)
                         .get(Region.F.id))
                 .value(regionIds);
     }
@@ -66,27 +68,28 @@ public class AdSpecs {
     public Specification<Ad> districtIdIn(Set<Long> cityIds) {
         return (root, query, cb) -> CollectionUtils.isEmpty(cityIds)
                 ? cb.conjunction()
-                : cb.in(root.get(Ad.F.user)
-                        .get(User.F.cityOfResidence)
-                        .get(City.F.region)
-                        .get(Region.F.district)
+                : cb.in(root.join(Ad.F.user)
+                        .join(User.F.cityOfResidence)
+                        .join(City.F.region)
+                        .join(Region.F.district)
                         .get(District.F.id))
                 .value(cityIds);
     }
 
-    public Specification<Ad> tagsIn(Set<TagDto> tags) {
+    public Specification<Ad> tagsIn(Set<TagDto> tagDtos) {
         return (root, query, cb) -> {
-            if (CollectionUtils.isEmpty(tags)) {
+            if (CollectionUtils.isEmpty(tagDtos)) {
                 return cb.conjunction();
             }
+
             var predicates = new HashSet<Predicate>();
-            for (TagDto tag : tags) {
+            for (TagDto tag : tagDtos) {
                 var tagAndCompetenceLevelPredicate = new ArrayList<Predicate>(2);
                 tagAndCompetenceLevelPredicate.add(
-                        cb.equal(root.get(Ad.F.tags).get(AdCompetenceLevelTag.F.tag).get(Tag.F.id), tag.getId()));
+                        cb.equal(root.join(Ad.F.tags).join(AdCompetenceLevelTag.F.tag).get(Tag.F.id), tag.getId()));
                 if (tag.getCompetenceLevel() != null) {
                     var predicate = cb.greaterThanOrEqualTo(
-                            root.get(Ad.F.tags).get(AdCompetenceLevelTag.F.competenceLevel), tag.getCompetenceLevel());
+                            root.join(Ad.F.tags).get(AdCompetenceLevelTag.F.competenceLevel), tag.getCompetenceLevel());
                     tagAndCompetenceLevelPredicate.add(predicate);
                 }
                 predicates.add(cb.and(tagAndCompetenceLevelPredicate.toArray(new Predicate[0])));
